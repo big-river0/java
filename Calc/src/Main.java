a2566ab583157855bc0c2205e3d47a7d360707aa
big-river <ppttkk9900@gmail.com>
2025-04-01 11:18:50 +0900

    Calc 추가

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int num1, num2;
+        int num1, num2, num3;
+        String cal;
         System.out.print("숫자1를 입력하세요: ");
         num1 = sc.nextInt();
         System.out.print("숫자2를 입력하세요: ");
         num2 = sc.nextInt();
+        System.out.print("숫자3를 입력하세요: ");
+        num3 = sc.nextInt();
 
-        System.out.println(num1 + num2);
-        }
+        System.out.println(num1 - num3 - num2);
     }
+}
