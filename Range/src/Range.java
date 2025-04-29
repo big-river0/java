dffa97c6add0e765bee3183075fe4a6c96debbbc
big-river <ppttkk9900@gmail.com>
2025-04-29 10:15:42 +0900

    Range 파일 추가

---
+++ Range/src/Range.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Range {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        System.out.println("정수를 입력하세요: ");
+        num = sc.nextInt();
+        if (num >= 0 && num <= 100) {
+            System.out.println("1에서 100 사이의 값입니다.");
+        }else {
+            System.out.println(("1에서 100 사이의 값이 아닙니다."));
+            }
+        }
+    }
