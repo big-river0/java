5a07dddf728eba9b3818b7428e592ced81137a1f
big-river <ppttkk9900@gmail.com>
2025-05-13 11:12:51 +0900

    oddsum 추가

--- oddsum/src/OddSum.java
+++ oddsum/src/OddSum.java
@@ -4,7 +4,7 @@ public class OddSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = 0;
-        System.out.print("숫자를 입력하세요: ");
+        System.out.println("숫자를 입력하세요: ");
         int max_num = sc.nextInt();
         if (max_num < 0) {
             System.out.println("잘못된 입력입니다.");
